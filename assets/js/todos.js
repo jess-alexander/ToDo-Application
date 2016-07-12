ac6785a4a534e86f1d658ac135901680f0cb15d6
jess-alexander <alexanderjessmarie@gmail.com>
2016-07-12 10:21:48 +0200

    Exchange long code for toggle class
    This is DRY code, and also separates styling and logic

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,5 @@
 
 
 $("li").click(function(){  // check off specific todos by clicking
-
-	//console.log($(this).css("color"));//console.log to see what the css color attribute returns exactly 
-
-	if($(this).css("color")==="rgb(128, 128, 128)"){ //if li is grey, turn it black
-		$(this).css({  //create an object 
-		color: "black",
-		textDecoration: "none" //use Javascript friendly camelCase to define text-Decoration
-		})
-	}else{//else, turn it grey
-		$(this).css({  //create an object 
-		color: "grey",
-		textDecoration: "line-through" //use Javascript friendly camelCase to define text-Decoration
-	})
-	}
-		
+	$(this).toggleClass("completed");
 });

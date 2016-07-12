deeefd02f0c9914ef429561af50d04e98151aaea
jess-alexander <alexanderjessmarie@gmail.com>
2016-07-12 11:52:12 +0200

    Add new element to page
    **error exists, click events do not fire on elements that are created
    after page load

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,25 @@
 $("li").click(function(){  // check off specific todos by clicking
 	$(this).toggleClass("completed");
 });
+
+$("span").click(function(event){ //click on X to delete ToDoß
+	event.stopPropagation(); //stop bubble effect of also firing li click event
+
+	$(this).parent().fadeOut(500,function(){ //fadeout the li, not the span
+		$(this).remove(); //this is referring to the li, the parent of the click event
+	});
+});
+
+$("input[type='text']").keypress(function(){
+	
+	if(event.which === 13){
+		var todoText = $(this).val();//grab new toDo text from input
+
+		$(this).val("");	//clear input
+		$("ul").append("<li><span>X </span>" + todoText + "</li>"); //add a new LI to insert input into the HTML
+
+		
+	}
+	
+
+})
